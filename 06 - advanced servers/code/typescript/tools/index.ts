cf2a7a38e92b73f0d52a5f6cd83cac54db435cb5
Rohit Singh <rohitsingh@packtpub.com>
2025-11-18 17:31:05 +0530

    Add files via upload

---
+++ 06 - advanced servers/code/typescript/tools/index.ts
@@ -0,0 +1,9 @@
+import addTool from "./add.js";
+import subtractTool from "./subtract.js";
+import {server} from "../server.js";
+import { Tool } from "./tool.js";
+
+export let tools: Array<Tool> = [];
+tools.push(addTool);
+tools.push(subtractTool);
+
---
+++ 06 - advanced servers/code/typescript/tools/tool.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export interface Tool {
+    name: string;
+    inputSchema: any;
+    rawSchema: z.ZodTypeAny;
+    callback: (args: z.infer<z.ZodTypeAny>) => Promise<{ content: { type: string; text: string }[] }>;
+}
